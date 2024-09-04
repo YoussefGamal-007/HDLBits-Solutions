1e7242c60a3e23e6e0128bb46279affc9d363562
Youssef Gamal Eldein Zaki <131666523+YoussefGamal-007@users.noreply.github.com>
2024-09-05 01:05:51 +0300

    Delete 2_Verilog Language directory

--- 2_Verilog Language/Basics/FourWires.v
+++
@@ -1,9 +0,0 @@
-module top_module( 
-    input a,b,c,
-    output w,x,y,z );
-
-    assign w = a,
-           x = b,
-           y = b,
-           z = c;
-endmodule
--- 2_Verilog Language/Modules Hierarchy/ThreeModules.v
+++
@@ -1,7 +0,0 @@
-module top_module ( input clk, input d, output q );
-
-    wire q1,q2;
-    my_dff ff1 ( clk,  d,  q1 );
-    my_dff ff2 ( clk, q1,  q2 );
-    my_dff ff3 ( clk, q2,  q  );
-endmodule
